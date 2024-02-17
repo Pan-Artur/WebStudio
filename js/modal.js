5d82fde5b93d9d88075279e20fd3cde1cc717d8d
Pan-Artur <132143853+Pan-Artur@users.noreply.github.com>
2024-02-17 10:02:41 +0200

    mobile menu finish

--- js/modal.js
+++ js/modal.js
@@ -8,7 +8,7 @@
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
 
-    function toggleModal(e) {
+    function toggleModal (e) {
       refs.modal.classList.toggle("is-hidden");
       document.body.classList.toggle("no-scroll");
     }
